726e07755cb25ed057e8a318314e38f8e0c3530e
benjoM <matijevicbenjamin@gmail>
2017-09-02 00:36:35 +0200

    verifikacija admina

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -9,8 +9,13 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Admin;
+use AppBundle\Form\AdminType;
+use AppBundle\Repository\AdminRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
@@ -24,5 +29,33 @@ class SecurityController extends Controller
             'last_username' => $helper->getLastUsername(),
             'error' => $helper->getLastAuthenticationError(),
         ]);
+    }
+    public function adminPage(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $admin= new Admin();
+        $form = $this->createForm(AdminType::class, $admin);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $username = $form->get('username')->getData();
+            $password = $form->get('password')->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $adminrepo= new AdminRepository($em, Admin::class);
+
+            $encoded = $encoder->encodePassword($admin, $password);
+
+            $enteredAdmin = $adminrepo->loadUserByUsername();
+
+            if ($enteredAdmin->getPassword() == $encoded ) {
+                return $this->redirectToRoute('admin_add');
+            }
+
+            return $this->redirectToRoute('task_success');
+        }
+        return $this->render('default/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+
     }
 }
